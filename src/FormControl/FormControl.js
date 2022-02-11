745e4c005df039e4393454873083ed5fc9f88cd9
Ramajeyam <jeyamjeyam6@gmail.com>
2022-02-11 14:45:51 +0530

    simplpified

--- src/FormControl/FormControl.js
+++ src/FormControl/FormControl.js
@@ -1,7 +1,8 @@
 import React, { Component }  from "react";
 
 import {initialState} from './Components/metaData';
-import Validation from './Components/Validation';
+import ChangeHandler from './Components/changeHandler';
+import HandleSubmit from './Components/HandleSubmit';
 import Input from './Components/Input';
 import Textarea from './Components/Textarea';
 import Dropdown from './Components/Dropdown';
@@ -16,119 +17,31 @@ export default class FormControl extends Component {
         this.state = {
             formIsValid: false,
             formControls : initialState
-            }
+        }
     }
     
 
-    changeHandler = event => {
-    
-        const name = event.target.name;
-        const updatedControls = { ...this.state.formControls }; //all values in formcontrols
-        const updatedFormElement = { ...updatedControls[name] }; // in formcontrols stores only the value where the change is happened
-      
-  
-        let value; //used to store the values of where the change is happened
-        let selectedOptions; //used to store the values of where the change is happened
-        let newValArray; //used to store the values of where the change is happened
-  
-        if (updatedControls[name].value instanceof Array &&
-            updatedControls[name].type === 'select' )                 //check if the change is happened in dropdown box
-        {
-            selectedOptions = event.target.selectedOptions;
-            // newValArray = [...selectedOptions].map(option => option.value);
-            value = selectedOptions.value;
-        }
-        else 
-        {
-            value = event.target.value;
+    handleChange = (event) => {
+        let change = ChangeHandler(event, this.state.formControls);
 
-            if( updatedControls[name].value instanceof Array &&
-                updatedControls[name].type === 'checkbox' )         //check if the change is happened in checkbox box
-            {
-                if(updatedControls[name].value.indexOf(value) > -1)  // ckecks if checkbox value is having the same value or not using indexof array method
-                {
-                    newValArray = updatedControls[name].value.filter(s => s !== value) // filter and stores in newValArray except the same value that is passed 
-               
-                } 
-                else 
-                {
-                    newValArray = [...updatedControls[name].value, value]; // if no duplicate values passed then using spread concat the values
-                }
-                value = newValArray; // stores the values in newValArray into value
-            }
-            else    //else the values are stored in values 
-            {
-                value = event.target.value
-            }
-        }
-       
-        updatedFormElement.value = value; // change the value  which has been changed and stores in locally
-        updatedFormElement.touched = true;
-  
-        let ValidationResult = Validation(value, updatedFormElement.validationRules);
-        updatedFormElement.valid = ValidationResult.valid; // change the valid  which has been changed and stores in locally
-
-        if ((!updatedFormElement.valid) && updatedFormElement.touched) //for update the error message value
-        {
-          updatedFormElement.errorMsg = ValidationResult.errorMsg // change the errorMsg  which has been changed and stores in locally
-        }
-        else 
-        {
-          updatedFormElement.errorMsg = ''
-        }
-  
-        updatedControls[name] = updatedFormElement; // change the local data where the change is happened
-  
-        let formIsValid = true;
-
-        for (let inputIdentifier in updatedControls) 
-        {
-          formIsValid = updatedControls[inputIdentifier].valid && formIsValid;
-        }
-  
         this.setState(
             {
-                formControls: updatedControls, // here we change the global value according to local data
-                formIsValid: formIsValid
+                formControls: change.updatedControls, 
+                formIsValid: change.formIsValid
             }
         );
-  
+ 
     }
 
     formSubmitHandler = () => {
-        const formData = {};
-        const touched = {};
-        const updatedControls = { ...this.state.formControls }; 
-        let updatedFormElement = { };
 
-        for (let formElementId in this.state.formControls) 
-        {
-            touched[formElementId] = this.state.formControls[formElementId].touched;
-            formData[formElementId] = this.state.formControls[formElementId].value; // in formData{} stores only values of each datas
-        }
+        let submit = HandleSubmit(this.state.formControls);
 
-        const delselect =  Object.keys(touched).filter((f) => touched[f] === false); 
-
-        if(delselect.length !== 0){
-            for(let object in delselect ){
- 
-                updatedFormElement = { ...updatedControls[delselect[object]] };
-                updatedFormElement.errorMsg = `Oops you missed ${delselect[object]} !! `
-                updatedControls[delselect[object]] = updatedFormElement;
-            }
-        }
-        else {
-            let newLine = '\r\n';
-            alert(
-            ` Name : ${formData.name}${newLine} Age : ${formData.age}${newLine} Email : ${formData.email}${newLine} Mobile : ${formData.mobile}${newLine} Address : ${formData.line1}, ${formData.line2}, ${formData.city}, ${formData.state}, ${formData.zipcode} - ${formData.country}${newLine} What would you use the library for : ${formData.radio}${newLine} Which sections of the library would you like access to : ${formData.checkbox}${newLine} About : ${formData.about} `
-            );
-        }
-        
         this.setState(
             {
-                formControls: updatedControls
+                formControls: submit.updatedControls
             }
-        ); 
+        );  
     }
 
     handleClearForm = () => {
@@ -146,33 +59,33 @@ export default class FormControl extends Component {
         return(
             <div>
                 {/* Name Field */}
-                <Input handleChange={this.changeHandler} values={this.state.formControls.name} /> 
+                <Input handleChange={this.handleChange} values={this.state.formControls.name} /> 
 
-                <Input handleChange={this.changeHandler} values={this.state.formControls.age} />
+                <Input handleChange={this.handleChange} values={this.state.formControls.age} />
 
-                <Radio handleChange={this.changeHandler} values={this.state.formControls.gender} /> 
+                <Radio handleChange={this.handleChange} values={this.state.formControls.gender} /> 
 
-                <Input handleChange={this.changeHandler} values={this.state.formControls.email} />
+                <Input handleChange={this.handleChange} values={this.state.formControls.email} />
 
-                <Input handleChange={this.changeHandler} values={this.state.formControls.mobile} />
+                <Input handleChange={this.handleChange} values={this.state.formControls.mobile} />
 
-                <Input handleChange={this.changeHandler} values={this.state.formControls.line1} />
+                <Input handleChange={this.handleChange} values={this.state.formControls.line1} />
 
-                <Input handleChange={this.changeHandler} values={this.state.formControls.line2} />
+                <Input handleChange={this.handleChange} values={this.state.formControls.line2} />
 
-                <Input handleChange={this.changeHandler} values={this.state.formControls.city} />
+                <Input handleChange={this.handleChange} values={this.state.formControls.city} />
 
-                <Input handleChange={this.changeHandler} values={this.state.formControls.state} />
+                <Input handleChange={this.handleChange} values={this.state.formControls.state} />
 
-                <Input handleChange={this.changeHandler} values={this.state.formControls.zipcode} />
+                <Input handleChange={this.handleChange} values={this.state.formControls.zipcode} />
 
-                <Dropdown handleChange={this.changeHandler} values={this.state.formControls.country} /> <br/>
+                <Dropdown handleChange={this.handleChange} values={this.state.formControls.country} /> <br/>
 
-                <Radio handleChange={this.changeHandler} values={this.state.formControls.radio} /> <br/>
+                <Radio handleChange={this.handleChange} values={this.state.formControls.radio} /> <br/>
 
-                <Checkbox handleChange={this.changeHandler} values={this.state.formControls.checkbox} /> <br/>
+                <Checkbox handleChange={this.handleChange} values={this.state.formControls.checkbox} /> <br/>
 
-                <Textarea handleChange={this.changeHandler} values={this.state.formControls.about} />
+                <Textarea handleChange={this.handleChange} values={this.state.formControls.about} />
 
                 <Buttong 
                     action = {this.formSubmitHandler}
